e5697caf6f20eda6a3aeeca673c03939980d617b
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-07-22 00:04:00 +0300

    Tests for evaluation of λ-terms without application.

--- spec/lambda.spec.js
+++ spec/lambda.spec.js
@@ -1,8 +1,22 @@
 import lambda from './lib/lambda'
 
-describe('lambda', () => {
+describe('lambda calculus', () => {
 
   it('should be defined', () => {
     expect(lambda).toBeDefined();
+  });
+
+  describe('variable', () => {
+
+    it('should evaluate to self', () => {
+      expect(lambda.eval('x')).toEqual('x');
+    });
+  });
+
+  describe('function', () => {
+
+    it('should evaluate to self', () => {
+      expect(lambda.eval('(λx.y)')).toEqual('(λx.y)');
+    });
   });
 });
