d472c7282cc08fe5200cf78e09cbe88a8fefe69d
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-07-25 23:16:39 +0300

    Expression parentheses support for parser.

--- spec/parser.spec.js
+++ spec/parser.spec.js
@@ -20,11 +20,44 @@ describe('parser', () => {
     expect(parser.parse('xy')).toEqual(new Application(new Variable('x'), new Variable('y')));
   });
 
-  //TODO: Should support parenthesis, nested excessive parenthesis
-  //TODO: Empty expression
-  //TODO: Handling errors, like incomplete expression like 'λ'
+  describe('parentheses', () => {
 
-  //(λx.(λy.(λz.zyx))
+    [
+      ['(x)', new Variable('x')],
+      ['(λx.x)', new Func('x', new Variable('x'))],
+      ['(xy)', new Application(new Variable('x'), new Variable('y'))],
+      ['(((x((y)))))', new Application(new Variable('x'), new Variable('y'))]
+    ].forEach(([expr, result]) => {
+      it(`should parse ${expr}`, () => {
+        expect(parser.parse(expr)).toEqual(result);
+      });
+    });
+
+    it('should throw an error if unmatched closing parentheses', () => {
+      try {
+        parser.parse('x)');
+        throw 'Ignored unmatched closing parenthesis';
+      } catch (e) {
+        expect(e).toEqual('Unmatched closing parenthesis in \'x)\' at position 2');
+      }
+    });
+
+    it('should throw an error if unmatched opening parentheses', () => {
+      try {
+        parser.parse('(x');
+        throw 'Ignored missing closing parenthesis';
+      } catch (e) {
+        expect(e).toEqual('Missing closing parenthesis in \'(x\' at position 2');
+      }
+    });
+  });
+
+  //TODO: Empty expression
+  //TODO: 'λ', 'λx', 'λx.' is an invalid expression
+  //TODO: '1' is and invalid expression
+  //TODO: '[x]' is an invalid expression, unknown symbol
+
+  //(λx.(λy.(λz.zyx)))
   xit('should parse nested functions 1', () => {
     
   });
