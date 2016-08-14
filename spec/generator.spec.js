63857ed84f8edc978b21cf4049e74bada2d3e2e1
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-08-14 21:40:48 +0300

    Initial tests for interpreter.

---
+++ spec/generator.spec.js
@@ -0,0 +1,1 @@
+//Spec for JavaScript code generator
---
+++ spec/interpreter.spec.js
@@ -0,0 +1,47 @@
+import { Interpreter, Variable, Func, Application } from './lib/lambda'
+
+describe('interpreter', () => {
+
+  let interpreter;
+
+  beforeEach(() => {
+    interpreter = new Interpreter();
+  });
+
+  const test = (fixtures) =>
+    fixtures.forEach(([expr, expected, comment = '']) => {
+      it(`should interpret ${expr} ${comment}`, () => {
+        expect(parser.parse(expr)).toEqual(expected);
+      });
+    })
+
+  describe('β-normal form: no further β-reduction is possible', () => {
+
+    test([
+      [
+        new Variable('x'),
+        'x',
+        'variable'
+      ],
+      [
+        new Func('x',
+          new Variable('y')
+        ),
+        '(λx.y)', 'function'
+      ],
+      [
+        new Application(
+          new Variable('x'),
+          new Variable('y')
+        ),
+        'xy', 'application'
+      ]
+    ]);
+  });
+
+  //TODO: One β-reduction remaining
+  //TODO: Several β-reductions
+  //TODO: Several pathes to do β-reduction
+  //TODO: Variable name overshadows a variable from an enclosing context: α-reduction is needed to rename the variables
+  //TODO: Evaluate f(Yf):  Y-combinator applied to a function, should be a fixed point
+});
