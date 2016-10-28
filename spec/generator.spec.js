df1e6191f500440e0fb70c7372d4b859c87fd356
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-10-28 21:49:20 +0300

    First tests for the generator.

--- spec/generator.spec.js
+++ spec/generator.spec.js
@@ -1,4 +1,52 @@
-//Spec for JavaScript code generator
+import { Generator } from 'generators/js/generator'
+import { Variable } from 'terms/variable'
+import { Func } from 'terms/function'
+import { Application } from 'terms/application'
+
+describe('generator', () => {
+
+  let generator;
+
+  beforeEach(() => {
+    generator = new Generator();
+  });
+
+  const test = fixtures =>
+    fixtures.forEach(([expr, expected, comment = '']) => {
+      it(`should generate code for ${expr} ${comment}`, () => {
+        expect(generator.eval(expr).toString()).toEqual(expected);
+      });
+    })
+
+  describe('simple term', () => {
+    test([
+      [
+        new Variable('x'),
+`function(x) {
+  return x;
+}`,
+        'variable'
+      ],
+      [
+        new Func('x', new Variable('y')),
+`function(x) {
+  return y;
+}`,
+        'function'
+      ],
+      [
+        new Application(
+          new Func('x', new Variable('y')),
+          new Variable('z')
+        ),
+`(function(x) {
+  return y;
+})(z)`,
+        'application'
+      ]
+    ]);
+  });
+});
 
 //TODO: Y-combinator Y = λf.((λx.f(xx))(λx.f(xx)))
 //TODO: Turing fixed point combinator Θ = (λxy.y((xx)y))(λxy.y((xx)y))
